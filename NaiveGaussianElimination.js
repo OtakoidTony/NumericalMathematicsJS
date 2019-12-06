2178d13d5b18c65b5054e3d3530d3133507ad17d
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-12-06 15:28:23 +0900

    Update NaiveGaussianElimination.js

--- NaiveGaussianElimination.js
+++ NaiveGaussianElimination.js
@@ -1,15 +1,28 @@
-var a=[[1,5,2],
-       [2,3,3],
-       [3,1,2]];
-var b=[1,
-       2,
-       3];
+var a=[[],
+       [0,6,-2,2,4],
+       [0,12,-8,6,10],
+       [0,3,-13,9,3],
+       [0,-6,4,1,-18]];
+var b=[0,16,26,-19,-34];
 
-/*
-integer i,j,k; real xmult; real array a, b
-*/
-var n = b.length;
-var i, j, k, xmult;
-for (var k = 1; k < n; i--) {
-  str = str + i;
+var x=[0,0,0,0,0];
+var n = 4;
+var k, i, j;
+
+for(k=1;k<=n-1;k++){
+    for(i=k+1;i<=n;i++){
+        for(j=k;j<=n;j++){
+            a[i][j]=a[i][j]-((a[i][k]/a[k][k])*a[k][j]);
+        }
+        b[i]=b[i]-((a[i][k]/a[k][k])*b[k]);
+    }
+}
+x[n]=b[n]/a[n][n];
+var sum;
+for(i=n-1;i>=1;i--){
+    sum=b[i];
+    for(j=i+1;j<=n;j++){
+        sum=sum-(a[i][j]*x[j]);
+    }
+    x[i]=sum/a[i][i];
 }
