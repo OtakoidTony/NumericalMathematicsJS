2300cbf9a2ad7ab3e14b553b24e9f115975630f3
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-12-06 13:55:31 +0900

    Update NaiveGaussianElimination.js

--- NaiveGaussianElimination.js
+++ NaiveGaussianElimination.js
@@ -1,6 +1,6 @@
-var a=[[1,5,2,3],
-       [2,3,3,2],
-       [3,1,2,2]];
+var a=[[1,5,2],
+       [2,3,3],
+       [3,1,2]];
 var b=[1,
        2,
        3];
@@ -8,7 +8,7 @@ var b=[1,
 /*
 integer i,j,k; real xmult; real array a, b
 */
-var n = b.length
+var n = b.length;
 var i, j, k, xmult;
 for (var k = 1; k < n; i--) {
   str = str + i;
